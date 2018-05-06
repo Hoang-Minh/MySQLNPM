9a1d88158a06aa66c0b6aa422d9966372cc54240
Hoang-Minh <quataoxanh83@gmail.com>
2018-05-05 17:13:26 -0700

    more sql queries with mysql npm

--- app.js
+++ app.js
@@ -9,9 +9,34 @@ var connection = mysql.createConnection({
 connection.connect(function(err){
   if(err) throw err;
   console.log("Connected");
+  //var q = "SELECT * FROM user";
+  // var q = `INSERT INTO user(username, pword, email, phone)
+  // VALUES("Barry", "password", "barry@abc.com", "123345")`
+
+  // var q = `INSERT INTO user SET ?`
   
-  connection.query("SELECT * FROM user", function(err, res){
+
+  // var values = {
+  //   username: "Someone2",
+  //   pword: "12345",
+  //   email: "someone@abc.com",
+  //   phone: 111111
+  // }
+  var q = `UPDATE user SET ? WHERE ?`
+  var values = [
+    {
+      pword: 22222
+    },
+    {
+      username: "Barry"
+    }
+  ]
+  connection.query(q, values, function(err, res){
     if(err) throw err;
     console.log(res);
+  });
+
+  connection.end(function(err){
+    if(err) throw err;
   })
 });
